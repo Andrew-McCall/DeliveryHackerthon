2c1714be434f8d14beb0df1eff17a88a6b018a57
Andrew-McCall <amccall@qa.com>
2022-08-29 21:52:38 +0100

    Fixed Login annotations

--- src/main/java/am/data/entites/Login.java
+++ src/main/java/am/data/entites/Login.java
@@ -1,13 +1,19 @@
 package am.data.entites;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
+@AllArgsConstructor
 public class Login {
 
 	private Long login_id;
+	@NonNull
 	private String username;
+	@NonNull
 	private String password;
+	@NonNull
 	private Boolean isManager;
 
 }
